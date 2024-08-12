0cf9b0de691dbdda2f78f8821e8419828dc2aa4f
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2024-08-12 18:38:32 +0200

    feat: main using the code. Surprise! The submodule has bugs

---
+++ main.py
@@ -0,0 +1,8 @@
+from submodule.main import print_number, get_random_numer
+
+print("Welcome to the amazing code that uses a submodule as a lib")
+print("We're using State of the art AI to generate you a random number...")
+
+print_number(get_random_numer())
+
+
